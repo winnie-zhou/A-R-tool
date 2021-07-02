b343ce1d2bdf86de7987e5a8d55057e2abc1d6d4
winnie-zhou <84091734+winnie-zhou@users.noreply.github.com>
2021-07-02 11:35:03 -0700

    Add files via upload

--- OpenCsvReader.java
+++ OpenCsvReader.java
@@ -4,13 +4,15 @@ import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
+import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.exceptions.CsvException;
-import java.io.FileInputStream;
+import java.lang.Object;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.*;
@@ -20,7 +22,26 @@ public class OpenCsvReader {
 	
     public static void main(String[] args) throws IOException, CsvException, URISyntaxException {
 
-        String fileName = "c:\\Users\\Winnie\\Downloads\\CSVDemo.csv";
+//        String fileName = "c:\\Users\\Winnie\\Downloads\\CSVDemo.csv";
+    	OpenCsvReader reader = new OpenCsvReader();
+    	String fileName = "c:\\Users\\Winnie\\Downloads\\Shazam Song Database.csv";
+    	String fileName1 = "c:\\Users\\Winnie\\Downloads\\Shazam Song Database1.csv";
+    	String fileName2 = "c:\\Users\\Winnie\\Downloads\\ShazamCount.csv";
+    	String file2 = "c:\\Users\\Winnie\\Downloads\\Shazam Top 200 United States Chart 30-05-2021.csv";
+    	String file = "c:\\Users\\Winnie\\Downloads\\Shazam Top 200 United States Chart 01-06-2021.csv";
+    	String[] arr = {"1", "2,", "3", "5"};
+    	String[] arr1 = {"1", "2", "3", "4"};
+    	
+    	System.out.println( reader.countCols(file2));
+    	
+    	reader.addColumn(file2, file, arr, true);
+//    	reader.copyOver(file, file2);
+//    	reader.addColumn(file2, file, arr1, true);
+ //   	reader.addRow(fileName, "hi");
+ //   	reader.addRow(fileName, "hi");
+ //   	reader.addSong(fileName, "HI");
+ //   	reader.addSong(fileName, "HI");
+ //   	reader.colToArr(fileName1);
  //       List<String[]> r = readCsvFile(fileName);
  //       r.forEach(x -> System.out.println(Arrays.toString(x)));
 
@@ -109,49 +130,140 @@ public class OpenCsvReader {
     	}
     	return ids;
     }
- /*   
-    public static List<String> getIds(String fileName) throws IOException, CsvException{
-    	List<String> ids = new ArrayList<String>(200);
-    	List<String[]> lines = readCsvFile(fileName); 
-    	for(int i = 0; i< lines.size()-3; i++) {
-    	   String[] line = lines.get(i+3);
- //   	   System.out.println(i);
- //    	   System.out.println(line[line.length-1]);
-     	   ids.add(line[line.length-1]);
+    
+    public int countCols(String fileName) throws IOException, CsvException{
+//    	BufferedReader br=null;
+    	try(CSVReader reader = new CSVReader(new FileReader(fileName))){
+    //		br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+    		String[] header = reader.readNext(); // assuming first read
+    		int columnCount = 0;
+    		if (header != null) {                     // and there is a (header) line
+    		   columnCount = header.length;       // get the column count
+    		//   System.out.println("number of columms: " + columnCount);
+    		}
+    		return columnCount;
     	}
-    	return ids;
+ //   		String line = br.readLine();
+ //   		line.split(",");
+    	
     }
-*/
-    public void addColumn(String fileName, String[] nums) throws IOException{
+    
+    public void addColumn(String fileName, String fileName1, String[] nums, boolean database) throws IOException{
+    	CSVReader reader = new CSVReader(new FileReader(fileName));
+    	CSVWriter writer = new CSVWriter(new FileWriter(fileName1));
+ //       final String lineSep=System.getProperty("line.separator");
+
+        try {
+          
+            String line = null;
+            int i=0;  
+
+            int numCols = countCols(fileName);
+            System.out.println("number of cols: " + numCols);
+            String[] header = reader.readNext();
+            while (header != null) 
+            { 
+ //           	System.out.println(line);
+                String addedColumn = String.valueOf(nums[i]);
+                
+                if(header.length == 3)
+            	{
+                	//line = "";
+                	String[] toAdd = new String[numCols+1];
+                	
+                	for (int k = 0; k<3; k++)
+                	{
+                		toAdd[k] = header[k];
+                	}
+                	
+                	for (int m = 3; m<numCols ; m++)
+                	{
+                		toAdd[m] = "";
+                	}
+                	
+                	toAdd[numCols] = addedColumn;
+                	
+                	for(int x = 0; x < numCols+1; x ++)
+                	{
+                		System.out.print(toAdd[x] + ",");
+                		
+                	}
+                	System.out.println();
+                	
+                	writer.writeNext(toAdd);
+            	}
+                else
+                {
+                	String[] toAdd1 = new String[numCols +1];
+                	for (int j = 0; j< numCols; j++)
+                	{
+                		toAdd1[j] = header[j];
+                	}
+                	toAdd1[numCols]= addedColumn;
+                	
+                	for(int x = 0; x < numCols+1; x ++)
+                	{
+                		System.out.print(toAdd1[x] + ",");
+                	
+                	}
+                	System.out.println();
+                	
+                	writer.writeNext(toAdd1);
+                }
+                i++;
+                header = reader.readNext();
+            }
+            reader.close();
+            writer.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }	
+
+/*    public void addColumn(String fileName, String fileName1, String[] nums, boolean database) throws IOException{
         BufferedReader br=null;
         BufferedWriter bw=null;
         final String lineSep=System.getProperty("line.separator");
 
         try {
             File file = new File(fileName);
-            File file2 = new File("c:\\Users\\Winnie\\Downloads\\ShazamChart6-10.csv");//so the
+            File file2 = new File(fileName1);//so the
                         //names don't conflict or just use different folders
 
-            br = new BufferedReader(new InputStreamReader(new FileInputStream(file))) ;
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
             bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file2)));
             String line = null;
-                        int i=0;
-          
-           
+                        int i=0;  
   //skip first 3 lines
-            String line1 = br.readLine();
-            bw.write(line1+lineSep);
-            String line2 = br.readLine();
-            bw.write(line2+lineSep);
-            String line3 = br.readLine();
-            bw.write(line3+"Song ID,Shazams" + lineSep);
+           if (database == false) {
+	            String line1 = br.readLine();
+	            bw.write(line1+lineSep);
+	            String line2 = br.readLine();
+	            bw.write(line2+lineSep);
+	            String line3 = br.readLine();
+	            bw.write(line3+"Song ID,Shazams" + lineSep);
+            }
+            else {}
+            int numCols = countCols(fileName);
+            System.out.println("number of cols: " + numCols);
             for ( line = br.readLine(); line != null; line = br.readLine(),i++)
-            {               
-
+            { 
+ //           	System.out.println(line);
                 String addedColumn = String.valueOf(nums[i]);
-                System.out.println(line+ "," + addedColumn + lineSep);
-                bw.write(line+ "," + addedColumn+lineSep); //add comma between?
-        }
+                
+                String[] cells = line.split(",");
+                
+                if(cells.length == 3)
+            	{
+                	//line = "";
+                	for(int j = 3; j < numCols; j++)
+                	{
+                		line = line.concat(",");
+                	}
+            	}
+                bw.write(line + "," + addedColumn+lineSep); //add comma between?
+            }
 
         }catch(Exception e){
             System.out.println(e);
@@ -162,9 +274,225 @@ public class OpenCsvReader {
                 bw.close();
         }
 
-    }	
-	
-	
-	
+    }	*/
+    
+    public void copyOver(String srcFile, String fileName) throws IOException
+    {
+    	File file = new File(fileName);
+    	FileOutputStream os = new FileOutputStream(file);
+    	Files.copy(Paths.get(srcFile), os);
+    }
+   
+    public void addCol (String fileName, String[] toAdd) throws IOException
+    {
+    	
+      	OutputStreamWriter fw=null;
+        BufferedWriter bw=null;
+        InputStreamReader fr = null;
+        BufferedReader br = null;
+        String s;
+        final String lineSep=System.getProperty("line.separator");
+    	try
+        {
+    		File file = new File(fileName);
+            fw= new OutputStreamWriter(new FileOutputStream(file));
+            bw=new BufferedWriter(fw);
+            try
+            {
+                fr=new InputStreamReader(new FileInputStream(file));
+                br=new BufferedReader(fr);
+                
+ /*               if (database == false) {
+    	            String line1 = br.readLine();
+    	            bw.write(line1+lineSep);
+    	            String line2 = br.readLine();
+    	            bw.write(line2+lineSep);
+    	            String line3 = br.readLine();
+    	            bw.write(line3+"Song ID,Shazams" + lineSep);
+                }else {}*/
+                int i = 0;
+                while((s=br.readLine())!=null)
+                {
+//                	String addedColumn = String.valueOf(toAdd[i]);
+                	String addedColumn = toAdd[i];
+                    if(s.equals(""))
+                 	{
+                 		s = ",";
+                 	}
+                    bw.write(s+ "," + addedColumn+lineSep);
+                    
+                    i++;
+                	
+                	s = br.readLine();
+                }
+                //bw.write(toAdd);
+                br.close();
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println("File was not found");
+            }
+            catch(IOException e)    
+            {
+                System.out.println("No file found");
+            }
+
+            bw.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Error1");
+        }
+    }    
+    
+    public int findNumRows(String fileName) throws IOException
+    {
+		BufferedReader br = null;
+    	
+    		int count = 0;
+	    		br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+	    		String input;
+		        while((input = br.readLine()) != null)
+		        {
+		            count++;
+		        }
+		        br.close();
+		        return count;
+    }
+    
+/*    public void addRow(String fileName, String fileName1, String toAdd) throws IOException
+    {
+        BufferedReader br=null;
+        BufferedWriter bw=null;
+        final String lineSep=System.getProperty("line.separator");     
+
+        try {
+            File file = new File(fileName);
+            File file2 = new File(fileName1);
+           
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file2)));
+          //  String line = null;
+            int i = 0;*/
+ /*           for ( line = br.readLine(); line != null; line = br.readLine(),i++)
+            {
+                bw.write(line); //add comma between?
+            }*/
+/*            String line = br.readLine();
+            while( line != null)
+            {
+            	bw.write(line + "\n");
+            	line = br.readLine();
+            }
+            bw.write(toAdd);
+        }catch(Exception e){
+            System.out.println(e);
+        }finally  {
+            if(br!=null)
+                br.close();
+            if(bw!=null)
+                bw.close();
+        }
+        
+    }*/
+    
+    public void addSong(String fileName, String toAdd)throws IOException
+    {
+    	try {
+	    	FileWriter writer = new FileWriter(fileName, true);
+	    	StringBuilder sb = new StringBuilder();
+	    	sb.append(toAdd);
+	    	sb.append('\n');
+	    	writer.write(sb.toString());
+//	    	writer.flush();
+	    	writer.close();
+    	}catch(FileNotFoundException e) {
+    		System.out.println(e.getMessage());
+    	}
+    }
+    
+    public void addRow(String fileName, String toAdd) throws IOException
+    {
+    	OutputStreamWriter fw=null;
+        BufferedWriter bw=null;
+        InputStreamReader fr = null;
+        BufferedReader br = null;
+        String s;
+    	try
+        {
+    		File file = new File(fileName);
+            fw= new OutputStreamWriter(new FileOutputStream(file));
+            bw=new BufferedWriter(fw);
+
+            try
+            {
+                fr=new InputStreamReader(new FileInputStream(file));
+                br=new BufferedReader(fr);
+
+                while((s=br.readLine())!=null)
+                {
+                	bw.write(s + "\n"); 
+                	
+                	s = br.readLine();
+                }
+                //bw.write(toAdd);
+                br.close();
+            }
+            catch(FileNotFoundException e)
+            {
+                System.out.println("File was not found");
+            }
+            catch(IOException e)    
+            {
+                System.out.println("No file found");
+            }
+
+            bw.close();
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Error1");
+        }
+    }
+    
+    
+    public String[] colToArr(String fileName, int colNum) throws IOException, CsvException {  //column of csv to array
+    	String[] lineInArray = new String[findNumRows(fileName)];
+         try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+        	 int i = 0;
+        	 String[] fullRow = reader.readNext();                    // and there is a (header) line
+          	   int columnCount = fullRow.length;       // get the column count
+        	
+        	// int numCols = columnCount;
+             while (fullRow != null) {
+            	// System.out.println(i);
+            	 
+            	 if (fullRow[colNum] == null){
+            		 lineInArray[i]= "0";
+            	 }else {
+            		 lineInArray[i] = fullRow[colNum]; //if empty, add 0
+            	 }
+//            	 System.out.println(i);
+   //         	 System.out.println(lineInArray[i]); 
+            	 i++;
+            	 fullRow = reader.readNext();
+             }
+             reader.close();
+         }
+         return lineInArray;
+
+     }	
+    
+    public int searchArray(String[] arr, int length, String toFind)
+    {
+    	int index = -1;
+    	for (int i=0;i<length;i++) {
+    	    if (arr[i].equals(toFind)) {
+    	        index = i;
+    	        break;
+    	    }
+    	}
+    	return index;
+    }
 	
 }
--- SongDataMain.java
+++ SongDataMain.java
@@ -1,5 +1,6 @@
 package com.songdata;
 import java.io.FileReader;
+
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
@@ -20,27 +21,94 @@ import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvException;
 import java.util.ArrayList;
 import java.lang.AutoCloseable;
+import java.util.Map;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SongDataMain {
-
+	
     public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
-    	
+    	//chartRequest();
 
-    	String file = "c:\\Users\\Winnie\\Downloads\\Shazam Top 200 United States Chart 10-06-2021 (1).csv";
+//    	String file = "c:\\Users\\Winnie\\Downloads\\Shazam Top 200 United States Chart 17-06-2021.csv";
+//    	String retFile = "c:\\Users\\Winnie\\Downloads\\ShazamCount.csv";
     	
-    	SongDataMain sdm = new SongDataMain();
+//    	String[] ids = new String[200];
     	OpenCsvReader reader = new OpenCsvReader();
-    	String[] ids = new String[200];
+    	String[] ids = chartRequest("\"key\":");
+    	String[] songNames = chartRequest("\"title\":");
+    	String[] artists = chartRequest("\"subtitle\":");
     	try {
-    	ids= reader.getIds(file);
+//    	ids= reader.getIds(file);
     	String[] shazams = new String[200];
-    	for (int i = 0; i< 200; i++)
+
+    	for (int i = 0; i< 200; i++) //change back to 200
     	{
     		String id = ids[i];
-    		System.out.println(i);
+    //		System.out.println(i);
     		shazams[i] = synchronousRequest(id);
+
     	}
-    	reader.addColumn(file, shazams);
+ //   	reader.addColumn(file, retFile, shazams, false);
+    	////DATABASE:
+    	String file1 = "c:\\Users\\Winnie\\Downloads\\Shazam Song Database.csv"; //
+    	String file2 = "c:\\Users\\Winnie\\Downloads\\Shazam Song Database1.csv";
+ //   	    	
+    	OpenCsvReader database = new OpenCsvReader(); 
+		int numSongs = database.findNumRows(file1);
+		
+		System.out.println("number of songs:" + numSongs);
+		String[] songIds = new String[numSongs]; // fix this
+		songIds = database.colToArr(file1, 0); 
+		
+		String[] ranks = new String[numSongs + 50];
+		ranks[0] = "Rank";
+		for (int k = 0; k <numSongs + 50; k++)
+		{
+			ranks[k] = "";
+		}
+
+    	int numRows = database.findNumRows(file1); //-1 bc date is on top
+    	
+    	int numRows1 = database.findNumRows(file1);
+    	System.out.println("NUMBER OF ROWS: "+ numRows);
+    	for (int i=0; i<200; i++){ //Searches for songs in daily top 200 that are not already in database
+    		int search = database.searchArray(songIds, numRows, ids[i]);
+    		//int search = Arrays.binarySearch(songIds, ids[i]); 
+    		//System.out.println(i + ":" + search);
+			if (search < 0) { 
+//				database.addRow(file1, file2, shazams[i]);
+				database.addSong(file1, ids[i] + "," + songNames[i] + "," + artists[i]);
+				ranks[numRows1] = Integer.toString(i+1);
+				numRows1++;
+				}
+			else {
+				String s = Integer.toString(i+1);
+				ranks[search] = s;
+			}
+    		}
+    	
+    	numRows = database.findNumRows(file1);
+    	String[] allSongIds = new  String[numRows];
+    	String[] allShazams = new String[numRows];
+    	System.out.println("NUMBER OF ROWS:"+ numRows);
+    	
+    	allSongIds = database.colToArr(file1, 0); //updates song ids
+    	LocalDate today = LocalDate.now();
+    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+    	
+    	allShazams[0] = formatter.format(today);
+    	for (int j = 1; j<numRows; j++) { //pulls shazam count for all songs 
+
+    		allShazams[j] = synchronousRequest(allSongIds[j]); 
+    		System.out.println(j);
     	}
+    	
+    	database.addColumn(file1, file2, allShazams, true);
+    	database.copyOver(file2, file1);
+    	database.addColumn(file1, file2, ranks, true);
+    	database.copyOver(file2, file1);
+    	}	
     	catch (FileNotFoundException e) {
     		e.printStackTrace();
     	}
@@ -50,7 +118,8 @@ public class SongDataMain {
     	catch(CsvException e) {
     		e.printStackTrace();
     	}
-
+    	
+    	
     }
 
 
@@ -91,31 +160,46 @@ public class SongDataMain {
         // use the client to send the request
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         		//new JsonBodyHandler<>(APOD.class)); 
-    	//<Supplier<APOD>>
-/*    	HttpURLConnection connection = (HttpURLConnection) new URL("https://www.shazam.com/services/count/v2/web/track/565106878").openConnection();
-		
-		connection.setRequestMethod("GET"); 
 
-		int response = connection.getResponseCode();
-        
-        if (response == 200){
-        	
-        	System.out.println(response);
-        }*/
         // the response:
       //  System.out.println(response);
-       System.out.println(response.body());        
+ //      System.out.println(response.body());        
     		String[] tokens = response.body().split("\"");
-/*    		int i = 0;
-    		while (tokens[i] != null)
-    		{
-    			System.out.println(tokens[i]);
-    		}*/
+
     		String str = tokens[6];
     		String shazams = str.substring(1, str.length() - 1);
 //    		System.out.println(shazams);
     		return shazams;
     
+    }
+    
+    private static String[] chartRequest(String find) throws IOException, InterruptedException
+    {
+    	var client = HttpClient.newHttpClient();
+
+        // create a request
+        var request = HttpRequest.newBuilder(
+            URI.create("https://www.shazam.com/shazam/v3/en-US/US/web/-/tracks/ip-country-chart-US?pageSize=200&startFrom=0")
+        ).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        String[] tokens1 = response.body().split(find);
+        String[] songList = new String[200];
+        
+        for (int i = 0; i<200; i++)
+        {
+        	String str = tokens1[i+1];
+        	String[] tokens2 = str.split("\"");
+        	String str1 = tokens2[1];
+        	if (str1.contains(",")) {
+        		songList[i] = "\"" + str1 + "\"";
+        	}
+        	else {
+        		songList[i] = str1;
+        	}
+  //      	System.out.println(songList[i]);
+        }
+        return songList;
     }
        
 
